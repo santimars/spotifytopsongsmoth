69c663576e2f46d3f4964150b366601af09c44d0
Santi Rodriguez <98999805+santimars@users.noreply.github.com>
2023-07-21 20:18:43 -0600

    Create listenyoursonghere.js

---
+++ listenyoursonghere.js
@@ -0,0 +1,12 @@
+const playlistId = '6h6peLumrtIZ1o0NgVGRUf';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/6h6peLumrtIZ1o0NgVGRUf?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
